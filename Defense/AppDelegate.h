ce2e9d271a9f07dcf1752664743f7128efea448b
Austin Shadel <Austin@Toshiba-2.local>
2016-10-16 18:35:58 -0700

    Initial Commit

---
+++ Defense/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Defense
+//
+//  Created by Austin Shadel on 10/16/16.
+//  Copyright © 2016 Austin Shadel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Defense/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Defense
+//
+//  Created by Austin Shadel on 10/16/16.
+//  Copyright © 2016 Austin Shadel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
